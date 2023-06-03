b23ddebc7b16d494d82acede519d29f14716b73e
Tom Sarantis <tsarantis@proton.me>
2023-06-03 23:44:28 +1000

    add resizing feature

--- Ishikawa Diagram.py
+++ Ishikawa Diagram.py
@@ -17,15 +17,12 @@ fig, ax = plt.subplots(figsize=(12, 6), layout='constrained')
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
 ax.axis('off')
-
 COLOR = 'C0'
-# Main spine
-main_spine = ax.plot([-4.01, 4], [0, 0], color=COLOR, linewidth=2)
 
 
 def problems(data: list,
              problem_x: float, problem_y: float,
-             prob_angle_x: float, prob_angle_y: float,):
+             prob_angle_x: float, prob_angle_y: float):
     """
     Draw each problem section of the Ishikawa plot.
 
@@ -58,7 +55,7 @@ def problems(data: list,
 
 
 def causes(data: list, cause_x: float, cause_y: float,
-           cause_xytext=(-14, -0.3), top: bool = True):
+           cause_xytext=(-10, -0.3), top: bool = True):
     """
     Place each cause to a position relative to the problems
     annotations.
@@ -71,8 +68,8 @@ def causes(data: list, cause_x: float, cause_y: float,
     cause_x, cause_y : float
         The `X` and `Y` position of the cause annotations.
     cause_xytext : tuple, optional
-        The distance of the cause text from the problem arrow in
-        fontsize units.
+        Adjust to set the distance of the cause text from the problem
+        arrow in fontsize units.
     top : bool
 
     Returns
@@ -96,7 +93,7 @@ def causes(data: list, cause_x: float, cause_y: float,
             cause_x -= coords[index][0]
             cause_y += coords[index][1][1]
 
-        ax.annotate(cause, xy=(cause_x, cause_y),
+        ax.annotate(cause, xy=(cause_x, cause_y), horizontalalignment='center',
                     xytext=cause_xytext,
                     fontsize='10',
                     xycoords='data',
@@ -121,41 +118,70 @@ def draw_body(*args):
     None.
 
     """
+    num = len(args)  # The number of problems
+    one_section = [3.5, 0.7]
+    two_sections = [1, -1]
+    three_sections = [-1.6, -2]
+
+    # Resize diagram to automatically scale in response to the number
+    # of problems in the input data.
+    if num == 1 or num == 2:
+        spine_length = (-2.01, 2)
+        head_pos = (2, 0)
+        tail_pos = ((-2.8, 0.8), (-2.8, -0.8), (-2.0, -0.01))
+        one_section[0] = 1.5
+    elif num == 3 or num == 4:
+        spine_length = (-3.01, 3)
+        head_pos = (3, 0)
+        tail_pos = ((-3.8, 0.8), (-3.8, -0.8), (-3.0, -0.01))
+        one_section = [2, 1.2]
+        two_sections[0] = -0.5
+        two_sections[1] = -1.2
+    else:  # num == 5 or 6
+        spine_length = (-4.01, 4)
+        head_pos = (4.01, 0)
+        tail_pos = ((-4.8, 0.8), (-4.8, -0.8), (-4.0, -0.01))
+        one_section[0] = 3.5
+        one_section[1] = 2.7
+        two_sections[0] = 1
+        two_sections[1] = 0.2
+        three_sections[0] = -1.6
+        three_sections[1] = -2.4
+
+    # Change the coordinates of the annotations on each loop.
     for index, problem in enumerate(args):
         top_row = True
         cause_arrow_y = 1.8
-        if index % 2 != 0:
-            top_row = False  # Plot problems below the spine
+        if index % 2 != 0:  # Plot problems below the spine
+            top_row = False
             y_prob_angle = -15.5
             cause_arrow_y = -1.8
+        else:  # Plot problems above the spine
+            y_prob_angle = 15
+        if index in (0, 1):
+            prob_arrow_x = one_section[0]
+            cause_arrow_x = one_section[1]
+        elif index in (2, 3):
+            prob_arrow_x = two_sections[0]
+            cause_arrow_x = two_sections[1]
         else:
-            y_prob_angle = 15  # Plot problems above the spine
-        if str(index) in '01':
-            prob_arrow_x = 3.5
-            cause_arrow_x = 2.7
-        elif str(index) in '23':
-            prob_arrow_x = 1
-            cause_arrow_x = 0.2
-        else:
-            prob_arrow_x = -1.6
-            cause_arrow_x = -2.4
+            prob_arrow_x = three_sections[0]
+            cause_arrow_x = three_sections[1]
         if index > 5:
             raise ValueError(f'Maximum number of problems is 6, you have entered '
                              f'{len(args)}')
 
+        # draw main spine
+        ax.plot(spine_length, [0, 0], color=COLOR, linewidth=2)
+        # draw fish head
+        ax.text(4.07, -0.08, 'PROBLEM', fontsize=12, color='white', weight='bold')
+        semicircle = Wedge(head_pos, 0.9, 270, 90, fc=COLOR)
+        ax.add_patch(semicircle)
+        # draw fishtail
+        triangle = Polygon(tail_pos, fc=COLOR)
+        ax.add_patch(triangle)
         problems(problem, prob_arrow_x, 0, -15, y_prob_angle)
         causes(problem, cause_arrow_x, cause_arrow_y, top=top_row)
-
-
-# draw fish head
-ax.text(4.07, -0.08, 'PROBLEM', fontsize=12, color='white', weight='bold')
-semicircle = Wedge((4.01, 0), 0.9, 270, 90, fc=COLOR)
-ax.add_patch(semicircle)
-
-# draw fishtail
-edges = ((-4.8, 0.8), (-4.8, -0.8), (-4.0, -0.01))
-triangle = Polygon(edges, fc=COLOR)
-ax.add_patch(triangle)
 
 
 # Input data
